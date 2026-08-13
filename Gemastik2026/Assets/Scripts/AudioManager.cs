bfc5c28a0aa4924fdaaedc7267e1540ea3971ca2
glycerinn <glynisdurdin07@gmail.com>
2026-08-13 19:31:44 +0700

    added audiomanager

--- Gemastik2026/Assets/Scripts/AudioManager.cs
+++ Gemastik2026/Assets/Scripts/AudioManager.cs
@@ -3,18 +3,68 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource BGM;
+    [SerializeField] AudioSource AMB;
     [SerializeField] AudioSource SFX;
 
     public AudioClip MainMenuBGM;
+
+    [Header("Ambience")]
+    public AudioClip LakeA;
+    public AudioClip MountainA;
+    public AudioClip VillageA;
+
+    [Header("Sidescroller")]
+    public AudioClip LakeBGM;
+    public AudioClip MountainBGM;
+    public AudioClip VillageBGM;
+
+    [Header("Banana")]
+    public AudioClip BananaBGM;
+    public AudioClip BananaFallsfx;
+    public AudioClip BananaCollectsfx;
+
+    [Header("Cook")]
+    public AudioClip CookBGM;
+    public AudioClip Restocksfx;
+    public AudioClip Submitsfx;
+    public AudioClip[] PlaceFoodsfx;
+
+    [Header("Fish")]
+    public AudioClip FishBGM;
+    public AudioClip Fishingsfx;
+    public AudioClip FishingSuccesssfx;
+    public AudioClip FishingFailsfx;
+
+    [Header("Nut")]
+    public AudioClip NutBGM;
+    public AudioClip Swipesfx;
+
+    [Header("Tempeh")]
+    public AudioClip TempeBGM;
+    public AudioClip Wrapsfx;
+
+    [Header("Rice")]
+    public AudioClip RiceBGM;
+
+    [Header("Trash/harvest/take")]
+    public AudioClip WinLoseBGM;
+    public AudioClip[] trashharvesttakesfx;
+
+    [Header("UI")]
+    public AudioClip BookClicksfx;
     public AudioClip Clicksfx;
-    public AudioClip[] playerattacksfx;
+    public AudioClip Doorsfx;
+    public AudioClip Hoversfx;
+    public AudioClip NewsClosesfx;
+    public AudioClip NewsOpensfx;
 
     private float savedCardMusicTime;
 
     public static AudioManager instance;
+
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
@@ -23,8 +73,12 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        
     }
+
+
+    // =========================
+    // BGM
+    // =========================
 
     public void playMainMenuBGM()
     {
@@ -32,17 +86,216 @@ public class AudioManager : MonoBehaviour
         BGM.Play();
     }
 
+    public void playLakeBGM()
+    {
+        BGM.clip = LakeBGM;
+        BGM.Play();
+    }
+
+    public void playMountainBGM()
+    {
+        BGM.clip = MountainBGM;
+        BGM.Play();
+    }
+
+    public void playVillageBGM()
+    {
+        BGM.clip = VillageBGM;
+        BGM.Play();
+    }
+
+    public void playBananaBGM()
+    {
+        BGM.clip = BananaBGM;
+        BGM.Play();
+    }
+
+    public void playCookBGM()
+    {
+        BGM.clip = CookBGM;
+        BGM.Play();
+    }
+
+    public void playFishBGM()
+    {
+        BGM.clip = FishBGM;
+        BGM.Play();
+    }
+
+    public void playNutBGM()
+    {
+        BGM.clip = NutBGM;
+        BGM.Play();
+    }
+
+    public void playTempeBGM()
+    {
+        BGM.clip = TempeBGM;
+        BGM.Play();
+    }
+
+    public void playRiceBGM()
+    {
+        BGM.clip = RiceBGM;
+        BGM.Play();
+    }
+
+    public void playWinLoseBGM()
+    {
+        BGM.clip = WinLoseBGM;
+        BGM.Play();
+    }
+
+
+    // =========================
+    // AMBIENCE
+    // =========================
+
+    public void playLakeA()
+    {
+        AMB.clip = LakeA;
+        AMB.Play();
+    }
+
+    public void playMountainA()
+    {
+        AMB.clip = MountainA;
+        AMB.Play();
+    }
+
+    public void playVillageA()
+    {
+        AMB.clip = VillageA;
+        AMB.Play();
+    }
+
+
+    // =========================
+    // BANANA
+    // =========================
+
+    public void playBananaFallSFX()
+    {
+        SFX.PlayOneShot(BananaFallsfx);
+    }
+
+    public void playBananaCollectSFX()
+    {
+        SFX.PlayOneShot(BananaCollectsfx);
+    }
+
+
+    // =========================
+    // COOK
+    // =========================
+
+    public void playRestockSFX()
+    {
+        SFX.PlayOneShot(Restocksfx);
+    }
+
+    public void playSubmitSFX()
+    {
+        SFX.PlayOneShot(Submitsfx);
+    }
+
+    public void playAttackSFX()
+    {
+        if (PlaceFoodsfx.Length == 0)
+            return;
+
+        int rand = Random.Range(0, PlaceFoodsfx.Length);
+
+        SFX.PlayOneShot(PlaceFoodsfx[rand]);
+    }
+
+
+    // =========================
+    // FISH
+    // =========================
+
+    public void playFishingSFX()
+    {
+        SFX.PlayOneShot(Fishingsfx);
+    }
+
+    public void playFishingSuccessSFX()
+    {
+        SFX.PlayOneShot(FishingSuccesssfx);
+    }
+
+    public void playFishingFailSFX()
+    {
+        SFX.PlayOneShot(FishingFailsfx);
+    }
+
+
+    // =========================
+    // NUT
+    // =========================
+
+    public void playSwipeSFX()
+    {
+        SFX.PlayOneShot(Swipesfx);
+    }
+
+
+    // =========================
+    // TEMPEH
+    // =========================
+
+    public void playWrapSFX()
+    {
+        SFX.PlayOneShot(Wrapsfx);
+    }
+
+
+    // =========================
+    // TRASH / HARVEST / TAKE
+    // =========================
+
+    public void playTrashHarvestTakeSFX()
+    {
+        if (trashharvesttakesfx.Length == 0)
+            return;
+
+        int rand = Random.Range(0, trashharvesttakesfx.Length);
+
+        SFX.PlayOneShot(trashharvesttakesfx[rand]);
+    }
+
+
+    // =========================
+    // UI
+    // =========================
+
+    public void playBookClickSFX()
+    {
+        SFX.PlayOneShot(BookClicksfx);
+    }
+
     public void playClickSFX()
     {
         SFX.PlayOneShot(Clicksfx);
     }
 
-    public void playAttackSFX()
-    {   
-        // if (isGameOver) return;
-        // if (isPaused) return;
-        int rand = Random.Range(0, playerattacksfx.Length);
-        SFX.PlayOneShot(playerattacksfx[rand]);
+    public void playDoorSFX()
+    {
+        SFX.PlayOneShot(Doorsfx);
     }
 
+    public void playHoverSFX()
+    {
+        SFX.PlayOneShot(Hoversfx);
+    }
+
+    public void playNewsCloseSFX()
+    {
+        SFX.PlayOneShot(NewsClosesfx);
+    }
+
+    public void playNewsOpenSFX()
+    {
+        SFX.PlayOneShot(NewsOpensfx);
+    }
 }
